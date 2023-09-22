f32327a56f3e521a5a0267d9ecae730b116c082d
yuliohh <143408559+yuliohh@users.noreply.github.com>
2023-09-22 00:27:20 -0600

    Sistema de Prueba 1
    Microrred con dos convertidores interconectados. Para resolver, ejecutar el archivo SOLVER_Numerico

---
+++ SOLVER_Numerico_inversorLCL_multi_pu.m
@@ -0,0 +1,109 @@
+%SOLVER inversorLCL_multi_pu numerico
+%clc
+clear all
+global Ri Li Rd Cf R L Rf Lf KP KI VCD IDREF IQREF MF Lg Rg 
+global Ccap kp_pll ki_pll Vp f num h
+global Vb Sb Ib Zb Anb wb Xb Lb Cb
+%Parametros de las n unidades vsc
+num=2;
+Rg=3.85278e-3;%0.65813e-3*2.20;
+Lg=0.102198e-3;%9.8969e-6*0.5;
+
+%vsc1
+Ri1=0.64533e-3;%0.1;
+Li1=0.435e-3;%1.2e-3;
+
+Rd1=6.3e-3;%2;
+Cf1=5.3e-3;%10e-6;
+
+R1=0.32266e-3;%0.1;
+L1=1.33e-6;%1e-3;
+
+%Impedancia del alimentador
+Rfd=0.4*Rg;
+Lfd=0.4*Lg;
+Rf1=0.2*Rg+Rfd;
+Lf1=0.2*Lg+Lfd;
+
+%vsc2
+Ri2=0.968e-3;%0.1;
+Li2=0.5135e-3;%1.2e-3;
+
+Rd2=0.2484e-3;%2;
+Cf2=8.7e-3;%10e-6;
+
+R2=0.70986e-3;%0.1;
+L2=0.6012e-6;%1e-3;
+
+Rf2=0.2*Rg;
+Lf2=0.2*Lg;
+
+%red electrica
+Ccap=120e-6;
+Rt1=Ri1+R1;
+Lt1=Li1+L1;
+
+Rt2=Ri2+R2;
+Lt2=Li2+L2;
+
+kp_pll=4;
+ki_pll=4e3;
+
+mf1=27;
+mf2=21;
+f=60;
+Vp=480*sqrt(2/3);
+Vcd1=1800;
+Vcd2=1800;
+
+Idref1=2*0.9*1e6/(3*Vp);
+Iqref1=0;
+
+Idref2=2*0.85*1.5e6/(3*Vp);
+Iqref2=0;
+
+h=110;
+tau=0.01e-3;
+kp1=4*Lt1/tau;
+ki1=4*Rt1/tau;
+
+kp2=4*Lt2/tau;
+ki2=4*Rt2/tau;
+PerD=10/100;
+
+%Valores base
+Vb=Vp/sqrt(2);
+Sb=1.5e6/3;
+Ib=Sb/Vb;
+Zb=Vb^2/Sb;
+Anb=1;
+wb=2*pi*f;
+Lb=Zb/wb;
+Cb=1/(Zb*wb);
+Xb=10e5;
+
+%Vectores con las cantidades de cada vsc en pu's
+Ri=[Ri1 Ri2]/Zb;
+Li=[Li1 Li2]/Lb;
+Rd=[Rd1 Rd2]/Zb;
+Cf=[Cf1 Cf2]/Cb;
+R=[R1 R2]/Zb;
+L=[L1 L2]/Lb;
+Rf=[Rf1 Rf2]/Zb;
+Lf=[Lf1 Lf2]/Lb;
+
+%%Ganancias modificadas para ver inestabilidad
+%KP=[kp1*200e-3 kp2];%
+%%Ganancias normalonas
+KP=[kp1 kp2];
+KI=[ki1 ki2];
+VCD=[Vcd1 Vcd2]/Vb;
+IDREF=[Idref1 Idref2]/Ib;
+IQREF=[Iqref1 Iqref2]/Ib;
+MF=[mf1 mf2];
+N=5001;%5001;
+u=[IDREF(1);IQREF(1);IDREF(2);IQREF(2)];
+tic
+[t1,y1]=rk4(@inversorLCL_multi_pu,[0 10/f],zeros(13*num+6,1),10*N,u);
+toc
+
---
+++ inversorLCL_multi_pu.m
@@ -0,0 +1,83 @@
+function dx=inversorLCL_multi_pu(t,x,u)
+%El vector x tiene 13*num+6 variables de estado, donde
+%num es la cantidad de inversores en paralelo
+%sistema modelado en pu
+
+global Ri Li Rd Cf R L Rf Lf KP KI VCD  MF Lg Rg 
+global Ccap kp_pll ki_pll Vp f num
+global Vb Sb Ib Zb Anb wb Xb Lb Cb
+IDREF=[u(1);u(3)];
+IQREF=[u(2);u(4)];
+
+w0=2*pi*f;
+vred=Vp*[sin(w0*t);sin(w0*t-2*pi/3);sin(w0*t+2*pi/3)];      
+AA=zeros(3,1);
+for k=1:num
+        ii=x(13*(k-1)+1:13*(k-1)+3);
+        vf=x(13*(k-1)+4:13*(k-1)+6);
+        ig=x(13*(k-1)+7:13*(k-1)+9);
+        gamma=x(13*(k-1)+10);
+        wi=x(13*(k-1)+11);
+        xind=x(13*(k-1)+12);
+        xinq=x(13*(k-1)+13);
+        v_cap=x(13*num+1:13*num+3);
+        it=x(13*num+4:13*num+6);
+        
+        Theta1=w0*t+Anb*gamma;
+
+        T1=-2/3*[cos(Theta1) cos(Theta1-2*pi/3) cos(Theta1+2*pi/3)];
+
+        C1=2/3*[sin(Theta1) sin(Theta1-2*pi/3) sin(Theta1+2*pi/3)
+                        cos(Theta1) cos(Theta1-2*pi/3) cos(Theta1+2*pi/3)];  
+
+        d_ig1=Rd(k)*wb/(Lf(k)+L(k))*ii-(Rd(k)+Rf(k)+R(k))*wb/(Lf(k)+L(k))*ig+vf*wb/(Lf(k)+L(k))-v_cap*wb/(Lf(k)+L(k));
+
+        vpcc1=Rf(k)*(ig)+Lf(k)/wb*d_ig1+v_cap;
+
+        vtpcc1=T1*vpcc1; %para la sincronizacion1
+
+        %componentes en dq de la corriente y el voltaje
+        idq1=C1*ii;
+        vred_dq1=C1*vpcc1;
+    
+        %err_d1=IDREF(k)-idq1(1);
+        %err_q1=IQREF(k)-idq1(2);
+
+        %ud1=KP(k)*err_d1/Zb+xind*Xb/Vb;
+        %uq1=KP(k)*err_q1/Zb+xinq*Xb/Vb;
+
+        Leq=Li(k)+L(k);
+
+        sd1=2/VCD(k)*(-w0*Leq*idq1(2)/wb+vred_dq1(1)+KP(k)*(IDREF(k)-idq1(1))/Zb+Xb*xind/Vb);
+        sq1=2/VCD(k)*(w0*Leq*idq1(1)/wb+vred_dq1(2)+KP(k)*(IQREF(k)-idq1(2))/Zb+Xb*xinq/Vb);
+
+        ma1=sqrt(sd1^2+sq1^2)/Vp;
+        theta1=atan2(sq1,sd1);
+
+        %moduladoras para  VSC1
+        modA1=ma1*sin(w0*t+theta1);
+        modB1=ma1*sin(w0*t+theta1-2*pi/3);
+        modC1=ma1*sin(w0*t+theta1+2*pi/3);
+        trian1=sawtooth(MF(k)*w0*t+1.5*pi,0.5);
+
+        sa1=(tanh(99*(modA1-trian1))+1)/2;
+        sb1=(tanh(99*(modB1-trian1))+1)/2;
+        sc1=(tanh(99*(modC1-trian1))+1)/2;
+        ss1=(sa1+sb1+sc1)/3;
+
+        g1=[sa1-ss1;sb1-ss1;sc1-ss1];
+        AA=AA+ig;
+        dx(13*(k-1)+1:13*k,1)=[-(Ri(k)+Rd(k))*wb/Li(k)*ii-wb/Li(k)*vf+Rd(k)*wb/Li(k)*ig+VCD(k)*wb/Li(k)*g1
+                                                wb*1/Cf(k)*ii-wb*1/Cf(k)*ig
+                                                d_ig1
+                                                -w0/Anb+wi*wb/Anb-kp_pll*vtpcc1*Vb/Anb
+                                                 -ki_pll*vtpcc1*Vb/wb
+                                                 KI(k)*(IDREF(k)-idq1(1))*Ib/Xb
+                                                 KI(k)*(IQREF(k)-idq1(2))*Ib/Xb];                                     
+end
+
+
+dx(13*num+1:13*num+6,1)=[wb*1/(Ccap/Cb)*(AA-it)
+                       -(Rg/Zb)*wb/(Lg/Lb)*it+v_cap*wb/(Lg/Lb)-(vred/Vb)*wb/(Lg/Lb)];
+                                            
+end
